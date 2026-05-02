ef78071645bce0cd76ea0143b5de43bdffcd9c6b
Markus Lewin <lewinmarkus@gmail.com>
2026-05-02 19:30:14 +0200

    Add web to apphost

--- apphost.cs
+++ apphost.cs
@@ -1,5 +1,10 @@
+﻿#:package Aspire.Hosting.JavaScript@13.2.4
 #:sdk Aspire.AppHost.Sdk@13.2.4
 
 var builder = DistributedApplication.CreateBuilder(args);
+
+builder
+  .AddJavaScriptApp("web", "./web")
+  .WithHttpEndpoint(env: "PORT");
 
 builder.Build().Run();

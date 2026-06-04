a4188d1094d9ccba22d41fde880f2c8cb5566aff
Markus Lewin <lewinmarkus@gmail.com>
2026-06-04 23:22:33 +0200

    Style /forgot-password

---
+++ SavingsTracker.Frontend/app/(auth)/forgot-password/page.css.ts
@@ -0,0 +1,8 @@
+import { colors } from "@/app/styles/sprinkles.css";
+import { rem } from "@/app/styles/utils";
+import { style } from "@vanilla-extract/css";
+
+export const paragraph = style({
+  maxInlineSize: rem(300),
+  color: colors["neutral-300"],
+});
--- SavingsTracker.Frontend/app/(auth)/forgot-password/page.tsx
+++ SavingsTracker.Frontend/app/(auth)/forgot-password/page.tsx
@@ -1,11 +1,53 @@
+import { paragraph } from "@/app/(auth)/forgot-password/page.css";
+import QuoteLayout from "@/app/(auth)/utils/quote-layout";
+import { Button } from "@/app/components/button";
+import { TextField } from "@/app/components/text-field";
+import { sprinkles } from "@/app/styles/sprinkles.css";
+import Form from "next/form";
 import Link from "next/link";
 
 const ForgotPassword = () => {
   return (
-    <>
-      <h1>Forgot password</h1>
-      <Link href={"/signin"}>Sign in</Link>
-    </>
+    <QuoteLayout>
+      <div
+        className={sprinkles({
+          stack: "space-0200",
+        })}
+      >
+        <h1>Forgot your password?</h1>
+        <p className={paragraph}>
+          Enter your email address and we&apos;ll send you a link to reset it.
+        </p>
+      </div>
+      <Form
+        className={sprinkles({
+          stack: "space-0250",
+        })}
+        action={async () => {
+          "use server";
+          console.log("Send reset link");
+        }}
+      >
+        <TextField label="Email address" name="email" isRequired />
+        <Button
+          className={sprinkles({
+            marginBlockStart: "space-0150",
+          })}
+        >
+          Send reset link
+        </Button>
+        <Link
+          className={sprinkles({
+            justifySelf: "center",
+            textAlign: "center",
+            color: "neutral-0",
+          })}
+          href={"/signin"}
+        >
+          Back to sign in
+        </Link>
+      </Form>
+    </QuoteLayout>
   );
 };
 

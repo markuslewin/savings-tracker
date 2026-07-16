91dc585a90d984dd4cd23eb8eb9430e019634cfe
Markus Lewin <lewinmarkus@gmail.com>
2026-07-16 22:14:28 +0200

    Deploy apps

---
+++ infra/workflow-provision.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export RESOURCE_GROUP="savings-tracker"
+export LOCATION="eastus"
+export REPO="markuslewin/savings-tracker"
+
+# Create resource group
+az group create --name "$RESOURCE_GROUP" --location "$LOCATION"
+
+# Create federated identity for workflow
+# Parse and set GitHub secrets for repo
+gh secret set --repo "$REPO" --env-file <(
+  az deployment group create \
+    --resource-group "$RESOURCE_GROUP" \
+    --template-file infra/workflow.bicep \
+    --parameters "repo=$REPO" \
+    --query properties.outputs.secrets.value \
+      | jq -r '.[] | "\(.name)=\(.secret)"'
+)
+
+# All resources are created inside of the created resource group. To delete:
+# az group delete --name $RESOURCE_GROUP

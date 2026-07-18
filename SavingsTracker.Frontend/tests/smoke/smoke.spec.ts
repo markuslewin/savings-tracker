d1fac308fdab9fb0417be1abce7ac8242a83f624
Markus Lewin <lewinmarkus@gmail.com>
2026-07-18 19:00:17 +0200

    Add smoke test

--- SavingsTracker.Frontend/tests/smoke/smoke.spec.ts
+++ SavingsTracker.Frontend/tests/smoke/smoke.spec.ts
@@ -9,19 +9,23 @@ test("can add and delete goal", async ({ page }) => {
     .getByRole("textbox", { name: "email" })
     .fill(process.env.PLAYWRIGHT_TESTUSER_EMAIL!);
   await page
-    .getByRole("textbox", { name: "email" })
+    .getByRole("textbox", { name: "password" })
     .fill(process.env.PLAYWRIGHT_TESTUSER_PASSWORD!);
   await page.getByRole("button", { name: "sign in" }).click();
-  await page.getByRole("button", { name: "create" }).click();
-  await page.getByRole("textbox", { name: "name" }).fill(faker.food.dish());
-  await page
+  await page.getByRole("button", { name: "new goal" }).click();
+
+  const dialog = page.getByRole("dialog", { name: "new goal" });
+  await dialog.getByRole("textbox", { name: "name" }).fill(faker.food.dish());
+  await dialog
     .getByRole("textbox", { name: "target" })
     .fill(faker.finance.amount());
-  await page.getByRole("button", { name: "deadline" }).click();
+  await dialog.getByRole("button", { name: "deadline" }).click();
   await page.getByRole("button", { name: "11" }).click();
-  await page.getByRole("button", { name: "create" }).click();
-  await page.getByRole("button", { name: "delete" }).click();
+  await dialog.getByRole("button", { name: "create" }).click();
   await page.getByRole("button", { name: "delete" }).click();
 
-  await expect(page).toHaveTitle("home");
+  const alertDialog = page.getByRole("alertdialog", { name: "delete" });
+  await alertDialog.getByRole("button", { name: "delete" }).click();
+
+  await expect(page).toHaveTitle(/home/i);
 });
